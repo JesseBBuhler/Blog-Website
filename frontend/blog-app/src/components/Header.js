2f4690e470b7a47559e4d2bf8be05302ac30d28c
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-07-28 13:34:10 -0600

    create dropdown menu and style header

--- frontend/blog-app/src/components/Header.js
+++ frontend/blog-app/src/components/Header.js
@@ -1,34 +1,64 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "./Header.css";
+// import logo from "/img/ThymeToGrowBlue.png";
 
 function Header() {
+  const [dropdown, setDropdown] = useState("hidden");
+  const toggleDropdown = () => {
+    setDropdown(dropdown == "hidden" ? "unhidden" : "hidden");
+  };
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/recipes">Recipes</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/">Youtube</a>
-          </li>
-          <li>
-            <a href="https://www.amazon.com/">Amazon Shop</a>
-          </li>
-        </ul>
+    <div className="headerContainer">
+      <div className="logoContainer">
+        <img src="/img/ThymeToGrowBanner.png" alt="logo"></img>
+      </div>
+      <nav className="dropdown">
+        <div className="dropdownBtn" onClick={toggleDropdown}>
+          Menu
+        </div>
+        <div className={`dropdownContent ${dropdown}`}>
+          <ul className="navList">
+            <li>
+              <Link onClick={toggleDropdown} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link onClick={toggleDropdown} to="/recipes">
+                Recipes
+              </Link>
+            </li>
+            <li>
+              <Link onClick={toggleDropdown} to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link onClick={toggleDropdown} to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link onClick={toggleDropdown} to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <a onClick={toggleDropdown} href="https://www.youtube.com/">
+                Youtube
+              </a>
+            </li>
+            <li>
+              <a onClick={toggleDropdown} href="https://www.amazon.com/">
+                Amazon Shop
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
-    </>
+    </div>
   );
 }
 
